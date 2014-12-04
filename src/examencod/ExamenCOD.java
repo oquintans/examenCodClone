206fdc1e3da2b7acc985ada2e345f2d8d39c1ef6
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2014-12-04 09:40:03 +0100

    Commit 4, revert

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,33 +1,32 @@
-//CLONE
+
 package examencod;
 
 public class ExamenCOD {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        int num = 11;
-        if (calcularPrimo(num)) {
-            System.out.println("Es numero primo");
-        } else {
-            System.out.println("NO es numero primo");
-        }
-
-        for (int cont = 2; cont < 1000; cont++) {
-            if (calcularPrimo(cont)) {
-                System.out.print(cont + " ");
-            }
-        }
-    }
-
-    public static boolean calcularPrimo(int variable) {
-        int aux = 2;
-        boolean result = true;
-        while ((result) && (aux != variable)) {
-            if (variable % aux == 0) {
-                result = false;
-            }
-            aux++;
-        }
-        return result;
-    }
-
+ int iN = 11;
+            	if (metodoMio(iN))
+System.out.println("Es numero primo");
+else
+ System.out.println("NO es numero primo");           	 
+          	 
+for (int x=2;x<1000;x++) {
+if (metodoMio(x))
+System.out.print(x + " ");                         	 
+}         	 
+	}
+public static boolean metodoMio(int variable){
+int j = 2;
+boolean aux=true;
+while ((aux) && (j!=variable)){
+if (variable % j == 0)
+aux = false;
+j++;
+}
+return aux;
+  } 	 
+    
 }
